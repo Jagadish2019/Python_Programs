4a66500e06512d69ce1065d86d29a03a88b9358a
Jagadish2019 <jagadishg2019@gmail.com>
2019-03-04 16:22:45 +0530

    First commit of add program

---
+++ Add.py
@@ -0,0 +1,12 @@
+# addition program
+
+x = int(input("enter x value:\t"))
+y = int(input("enter y value:\t"))
+
+print("x value is:",x)
+print("y value is:",y)
+
+c = x + y
+
+print(f"addition of {x} and {y} is:",c)
+
